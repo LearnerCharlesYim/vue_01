5db27d406c8ecdc48fdc60b19ddb51751d47cae4
Charlesyim <yanchicharles@gmail.com>
2021-11-09 17:53:09 +0800

    = =

--- src/main.js
+++ src/main.js
@@ -1,6 +1,75 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import {
+  Button,
+  Form,
+  FormItem,
+  Input,
+  Message,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Menu,
+  Submenu,
+  MenuItemGroup,
+  MenuItem,
+  Breadcrumb,
+  BreadcrumbItem,
+  Card,
+  Row,
+  Col,
+  Table,
+  TableColumn,
+  Switch,
+  Tooltip,
+  Pagination,
+  Dialog,
+  MessageBox
+} from 'element-ui'
+import './assets/css/global.css'
+import './assets/fonts/iconfont.css'
+import axios from 'axios'
+import qs from 'qs'
+
+axios.defaults.baseURL = 'http://localhost:90/'
+axios.interceptors.request.use(config => {
+  const token = sessionStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = token
+  }
+  config.data = qs.stringify(config.data)
+  return config
+})
+
+Vue.component(Button.name, Button)
+Vue.component(Form.name, Form)
+Vue.component(FormItem.name, FormItem)
+Vue.component(Input.name, Input)
+Vue.component(Container.name, Container)
+Vue.component(Header.name, Header)
+Vue.component(Main.name, Main)
+Vue.component(Aside.name, Aside)
+Vue.component(Menu.name, Menu)
+Vue.component(Submenu.name, Submenu)
+Vue.component(MenuItemGroup.name, MenuItemGroup)
+Vue.component(MenuItem.name, MenuItem)
+Vue.component(Breadcrumb.name, Breadcrumb)
+Vue.component(BreadcrumbItem.name, BreadcrumbItem)
+Vue.component(Card.name, Card)
+Vue.component(Row.name, Row)
+Vue.component(Col.name, Col)
+Vue.component(Table.name, Table)
+Vue.component(TableColumn.name, TableColumn)
+Vue.component(Switch.name, Switch)
+Vue.component(Tooltip.name, Tooltip)
+Vue.component(Pagination.name, Pagination)
+Vue.component(Dialog.name, Dialog)
+
+Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox.confirm
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,135 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <el-container class='home-container'>
+    <el-header>
+      <div>
+        <img src='../assets/heima.png' alt=''>
+        <span><router-link to='/home'>电商后台管理系统</router-link></span>
+      </div>
+      <el-button type='info' @click='logout'>注销</el-button>
+    </el-header>
+    <el-container>
+      <el-aside :width='isCollapse?"64px":"200px"'>
+        <div class='toggle-button' @click='toggleCollapse'>|||</div>
+        <el-menu :default-active='activePath' class='el-menu-vertical-demo' background-color='#333744' text-color='#fff'
+                 active-text-color='#409eff' :unique-opened='true' :collapse='isCollapse' :collapse-transition='false'
+                 :router='true'>
+          <el-submenu :index='item.id + ""' v-for='item in menuList' :key='item.id'>
+            <template slot='title'>
+              <i :class='iconsObj[item.id]'></i>
+              <span>{{ item.authName }}</span>
+            </template>
+            <el-menu-item :index='"/"+subItem.path' v-for='subItem in item.children' :key='subItem.id'
+                          @click='saveNavState("/"+subItem.path)'>
+              <template slot='title'>
+                <i class='el-icon-menu'></i>
+                <span>{{ subItem.authName }}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      menuList: [],
+      iconsObj: {
+        2: 'iconfont icon-user',
+        3: 'iconfont icon-tijikongjian',
+        1: 'iconfont icon-shangpin',
+        4: 'iconfont icon-danju',
+        5: 'iconfont icon-baobiao'
+      },
+      isCollapse: false,
+      activePath: ''
+    }
+  },
+  created () {
+    this.getMenuList()
+    this.activePath = sessionStorage.getItem('activePath')
+  },
+  methods: {
+    logout () {
+      sessionStorage.clear()
+      this.$router.push('/login')
+    },
+    async getMenuList () {
+      const { data: res } = await this.$http.get('rights/menus')
+      if (res.state !== 200) return this.$messagee.error(res.message)
+      this.menuList = res.data
+    },
+    toggleCollapse () {
+      this.isCollapse = !this.isCollapse
+    },
+    saveNavState (activePath) {
+      sessionStorage.setItem('activePath', activePath)
+      this.activePath = activePath
+    }
   }
 }
 </script>
+
+<style lang='less' scoped>
+.home-container {
+  height: 100%;
+}
+
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+
+  > div {
+    display: flex;
+    align-items: center;
+
+    span {
+      color: #fff;
+      margin-left: 15px;
+      a{
+        text-decoration: none;
+        color: #fff;
+      }
+    }
+  }
+}
+
+.el-aside {
+  background-color: #333744;
+
+  .el-menu {
+    border-right: none;
+  }
+}
+
+.el-main {
+  background-color: #eaedf1;
+}
+
+.iconfont {
+  margin-right: 10px;
+}
+
+.toggle-button {
+  background-color: #4a5064;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
+}
+</style>
